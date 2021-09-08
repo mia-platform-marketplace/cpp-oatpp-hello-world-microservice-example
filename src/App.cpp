a8d3d772f26851e3d9f888506a99fd99abcaf8be
Evange95 <33718134+Evange95@users.noreply.github.com>
2021-09-08 09:56:12 +0200

    Update src/App.cpp
    Co-authored-by: Federico Maggi <7142570+fredmaggiowski@users.noreply.github.com>

--- src/App.cpp
+++ src/App.cpp
@@ -56,7 +56,7 @@ int main(int argc, const char * argv[]) {
   run();
   
   /* Print how much objects were created during app running, and what have left-probably leaked */
-  /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
+  // Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance
   std::cout << "\nEnvironment:\n";
   std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
   std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
